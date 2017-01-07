4ddb2b04d8a835749ccf9ac7025ab16aff898e57
theMatus <matus.miglierini@gmail.com>
2017-01-07 15:35:22 +0100

    Tasky

--- app/src/main/java/com/thejuanandonly/schoolapp/NotificationRecieverActivity.java
+++ app/src/main/java/com/thejuanandonly/schoolapp/NotificationRecieverActivity.java
@@ -1,6 +1,5 @@
 package com.thejuanandonly.schoolapp;
 
-import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,12 +7,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.Icon;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.PowerManager;
-import android.os.Vibrator;
 import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -23,8 +22,14 @@ import java.util.Date;
 
 public class NotificationRecieverActivity extends BroadcastReceiver {
 
+    Context context;
+    Intent intent;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        this.context = context;
+        this.intent = intent;
+
         SharedPreferences prefs = context.getSharedPreferences("ListOfTasks", Context.MODE_PRIVATE);
         JSONArray arrayName, arrayWhat, arrayTime;
 
@@ -58,9 +63,33 @@ public class NotificationRecieverActivity extends BroadcastReceiver {
             }
         }
 
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, NotificationRecieverActivity.class), 0);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 60000, pendingIntent);
+        new runInBackground().execute();
+    }
+
+    final Handler handler = new Handler();
+    private class runInBackground extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(final Void unused) {
+            handler.postDelayed(new Runnable() {
+
+                public void run() {
+                    onReceive(context, intent);
+                }
+            }, 30000);
+
+            this.cancel(false);
+        }
     }
 
     public void notifyUser(Context context, String name, String what) {
@@ -70,41 +99,27 @@ public class NotificationRecieverActivity extends BroadcastReceiver {
                 s = prefs.getBoolean("sounds", true),
                 v = prefs.getBoolean("vibrations", true);
 
-        if (n == true) {
+
+        if (n) {
             Intent intent = new Intent(context, MainActivity.class);
             intent.putExtra("fromNotification", true);
             PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            Notification notification = null;
-            if (!s && !v) {
-                notification = new Notification.Builder(context)
-                        .setContentTitle(name)
-                        .setContentText(what)
-                        .setVibrate(new long[]{500, 500, 500, 500})
-                        .setSmallIcon(R.drawable.ic_event_available_white_24dp)
-                        .setContentIntent(contentIntent).build();
-            } else if (!v && !s) {
-                notification = new Notification.Builder(context)
-                        .setContentTitle(name)
-                        .setContentText(what)
-                        .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
-                        .setSmallIcon(R.drawable.ic_event_available_white_24dp)
-                        .setContentIntent(contentIntent).build();
-            } else if (!s && !v) {
-                notification = new Notification.Builder(context)
-                        .setContentTitle(name)
-                        .setContentText(what)
-                        .setSmallIcon(R.drawable.ic_event_available_white_24dp)
-                        .setContentIntent(contentIntent).build();
-            } else {
-                notification = new Notification.Builder(context)
-                        .setContentTitle(name)
-                        .setContentText(what)
-                        .setVibrate(new long[]{500, 500, 500, 500})
-                        .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
-                        .setSmallIcon(R.drawable.ic_event_available_white_24dp)
-                        .setContentIntent(contentIntent).build();
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setContentTitle(name);
+            builder.setContentText(what);
+            builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_pls));
+            builder.setSmallIcon(R.drawable.ic_event_available_white_24dp);
+            builder.setContentIntent(contentIntent);
+
+            if (v) {
+                builder.setVibrate(new long[]{500, 500, 500, 500});
             }
+            if (s) {
+                builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
+            }
+
+            Notification notification = builder.build();
 
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             notification.flags |= Notification.FLAG_AUTO_CANCEL;
